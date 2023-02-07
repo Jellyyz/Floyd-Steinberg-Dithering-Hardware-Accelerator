d4c5f37d671c7e1724dc0e65518ee07083bae21b
Gally Huang <gallyhuang@gmail.com>
2023-02-06 21:17:20 -0600

    added basic hdl

---
+++ FPGA/hdl/top.sv
@@ -0,0 +1,14 @@
+module top(
+    // CLk - Rst Interface 
+    input logic CLk, Rst 
+    input logic [7:0] red, green, blue 
+
+); 
+
+logic [31:0] png_data []
+
+// reading stuff from a file section of code
+
+
+
+endmodule 
