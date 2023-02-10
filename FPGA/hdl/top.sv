60ee922ec4cd5b401ac53b842062be4dddc8ceb8
Gally Huang <gallyhuang@gmail.com>
2023-02-09 20:51:37 -0600

    final changes

--- FPGA/hdl/top.sv
+++ FPGA/hdl/top.sv
@@ -1,14 +1,20 @@
 module top(
     // CLk - Rst Interface 
-    input logic CLk, Rst 
+    input logic clk, rst 
     input logic [7:0] red, green, blue 
 
 ); 
 
-logic [31:0] png_data []
+logic [31:0] png_data_red [7:0];
+logic [31:0] png_data_green [7:0];
+logic [31:0] png_data_blue [7:0];
 
-// reading stuff from a file section of code
+always_ff @(posedge clk or posedge rst) begin 
 
+    if(rst)
+        png_data_red <= '0; 
+    else
 
+end 
 
 endmodule 
