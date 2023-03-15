5610f9bc69c01cf1347ae040bd57cabd134cdb15
Mechpet <blazepet2@gmail.com>
2023-03-15 12:32:00 -0500

    Added SW imp update

--- Software Implementation/c_imp/floyd_steinberg.cpp
+++ Software Implementation/c_imp/floyd_steinberg.cpp
@@ -18,24 +18,25 @@ void floyd_steinberg_dithering_serial(vector<unsigned char> &img, unsigned int w
   */
   for (int y = 0; y < h; y++) {
     for (int x = 0; x < w; x++) {
-      for (int rgb = 0; rgb < 3; rgb++) {
-        unsigned char old_pixel = img[y * w * 3 + x * 3 + rgb];
-        unsigned char new_pixel = (old_pixel + 127) / 255;
-        img[y * w * 3 + x * 3 + rgb] = new_pixel * 255;
-        int quant_error = old_pixel - new_pixel * 255;
-        if (x + 1 < w) {
-          img[y * w * 3 + (x + 1) * 3 + rgb] += (quant_error * 7 / 16);
-        }
-        if (y + 1 < h) {
-          img[(y + 1) * w * 3 + x * 3 + rgb] += (quant_error * 5 / 16);
-          if (x - 1 > 0) {
-            img[(y + 1) * w * 3 + (x - 1) * 3 + rgb] += (quant_error * 3 / 16);
-          }
-          if (x + 1 < w) {
-            img[(y + 1) * w * 3 + (x + 1) * 3 + rgb] += (quant_error * 1 / 16);
-          }
-        }
+      unsigned char old_pixel = img[y * w + x];
+      img[y * w + x] = (old_pixel > 127) ? 255 : 0;
+      char quant_error = (char) old_pixel - (char) img[y * w + x];
+      if (x % w != w - 1) {
+        img[y * w + (x + 1)] += quant_error * (7 / 16);
+        //CLAMP(1, 0, 7);
       }
+      if (y != h - 1) {
+        if (x % w) {
+          img[(y + 1) * w + (x - 1)] += quant_error * (3 / 16);
+          //CLAMP(-1, 1, 3);
+        }
+        img[(y + 1) * w + x] += quant_error * (5 / 16);
+        //CLAMP(0, 1, 5);
+        if (x % w != w - 1) {
+          //CLAMP(1, 1, 1);
+          img[(y + 1) * w + (x + 1)] += quant_error * (1 / 16);
+        }
+      } 
     }
   }
 }
---
+++ Software Implementation/c_imp/floyd_steinberg.h
@@ -0,0 +1,16 @@
+#ifndef FLOYD_STEINBERG_H
+#define FLOYD_STEINBERG_H
+
+#include <vector>
+using namespace std;
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define CLAMP(dx, dy, num) if (quant_error > 0)\
+    img[(y + (dy)) * w + (x + (dx))] = img[(y + (dy)) * w + (x + (dx))] + quant_error * (num) / 16 < img[(y + (dy)) * w + (x + (dx))] ? 255 : img[(y + (dy)) * w + (x + (dx))] + quant_error * (num) / 16;\
+    else\
+    img[(y + (dy)) * w + (x + (dx))] = img[(y + (dy)) * w + (x + (dx))] + quant_error * (num) / 16 > img[(y + (dy)) * w + (x + (dx))] ? 0 : img[(y + (dy)) * w + (x + (dx))] + quant_error * (num) / 16;
+
+void floyd_steinberg_dithering_serial(vector<unsigned char> &img, unsigned int w, unsigned int h);
+
+#endif
